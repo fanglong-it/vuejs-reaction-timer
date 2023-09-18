ca47fb666d1024e0207cb95ee9968caac922288f
fanglong-it <fang.longpc@gmail.com>
2023-09-18 17:37:19 +0700

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,41 @@
+<!-- eslint-disable vue/no-unused-components -->
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1>Longpc reaction timer</h1>
+  <button @click="start" :disabled="isPlaying">play</button>
+  <Block v-if="isPlaying" :delay="delay" @end="endGame"/>
+  <Result v-if="showResult" :score="score"/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Block from './components/Block.vue';
+import Result from './components/Result.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Block,
+    Result,
+  },
+  data() {
+    return {
+      isPlaying: false,
+      delay: null,
+      score: null,
+      showResult: false,
+    }
+  },
+  methods:{
+    start: function(){
+      this.delay = 2000 + Math.random() * 5000;
+      this.isPlaying = true;
+      console.log(this.delay);
+      this.showResult = false;
+    },
+    endGame(reactionTime){
+      this.score = reactionTime;      
+      this.isPlaying = false;
+      this.showResult = true;
+    }
   }
 }
 </script>
@@ -22,5 +48,21 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+button{
+  background: #0faf87;
+  color: white;
+  border: none;
+  padding: 8px 16px;
+  border-radius: 4px;
+  font-size: 16px;
+  letter-spacing: 1px;
+  cursor: pointer;
+  margin: 10px;;
+}
+button[disabled]{
+  opacity: 0.2;
+  cursor: not-allowed;
 }
 </style>
